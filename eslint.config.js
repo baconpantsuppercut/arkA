38f7f99c7cf5202fa103cdbbeedd0d9731c4f672
Moshe Ben Peshe <6439152+baconpantsuppercut@users.noreply.github.com>
2025-11-15 22:30:46 -0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,10 @@
-import js from "@eslint/js";
+// eslint.config.js
+// Flat config for ESLint 9, using CommonJS so Node can load it without "type": "module".
 
-export default [
+const js = require("@eslint/js");
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+module.exports = [
   {
     ...js.configs.recommended,
     files: ["client/**/*.js", "scripts/**/*.js"],
